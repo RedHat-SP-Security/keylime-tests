49b5aa9c10d764c94f5a75b55bb150759a846f2a
Karel Srot <kkaarreell@users.noreply.github.com>
2021-09-14 14:56:36 +0200

    initial version of configure_kernel_ima_module (#1)
    * initial version of configure_kernel_ima_module
    
    * fix code indentation

---
+++ install/configure_kernel_ima_module/test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+[ -z "$IMA_STATE" ] && IMA_STATE="on"
+[ -z "$IMA_POLICY" ] && IMA_POLICY="tcb"
+
+COOKIE=/var/tmp/configure-kernel-ima-module-rebooted
+TESTFILE=/var/tmp/configure-kernel-ima-module-test$$
+
+rlJournalStart
+
+  if [ ! -e $COOKIE ]; then
+    rlPhaseStartSetup "pre-reboot phase"
+        rlRun "grubby --info DEFAULT | grep '^args'"
+        rlRun "grubby --update-kernel DEFAULT --args 'ima=$IMA_STATE ima_policy=$IMA_POLICY'"
+        rlRun -s "grubby --info DEFAULT | grep '^args'"
+        rlAssertGrep "ima=$IMA_STATE" $rlRun_LOG
+        rlAssertGrep "ima_policy=$IMA_POLICY" $rlRun_LOG
+        rlRun "touch $COOKIE"
+
+        rhts-reboot
+    rlPhaseEnd
+
+  else
+    rlPhaseStartTest "post-reboot IMA test"
+        rlRun -s "cat /proc/cmdline"
+        rlAssertGrep "ima=$IMA_STATE" $rlRun_LOG
+        rlAssertGrep "ima_policy=$IMA_POLICY" $rlRun_LOG
+        rlRun "rm $COOKIE"
+
+        if [ "$IMA_STATE" == "on" -o "$IMA_STATE" == "1" ]; then
+            rlRun "touch $TESTFILE && cat $TESTFILE && rm $TESTFILE"
+            rlRun "grep $TESTFILE /sys/kernel/security/ima/ascii_runtime_measurements"
+        fi
+    rlPhaseEnd
+  fi
+
+rlJournalEnd
