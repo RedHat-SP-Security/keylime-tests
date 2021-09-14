e5b9d3ceb2fe57dd5b28f20c62716ff2e80c13fd
Karel Srot <ksrot@redhat.com>
2021-09-14 14:53:53 +0200

    added install/install_upstream_keylime

---
+++ install/install_upstream_keylime/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+
+    rlPhaseStartSetup "Install keylime and its dependencies"
+        # configure Sergio's copr repo providing necessary dependencies
+        rlIsRHEL 9 && rlRun "dnf -y copr enable copr.devel.redhat.com/scorreia/keylime rhel-9.dev-$(arch)"
+        rlRun "yum -y install git-core python3-pip python3-pyyaml python3-tornado python3-simplejson python3-requests python3-sqlalchemy python3-alembic python3-packaging python3-psutil python3-gnupg python3-cryptography libselinux-python3 procps-ng tpm2-abrmd tpm2-tss tpm2-tools python3-zmq cfssl patch"
+        rlRun "rm -rf keylime && git clone https://github.com/keylime/keylime.git"
+        pushd keylime
+        rlRun "python3 setup.py install"
+        popd
+    rlPhaseEnd
+
+    rlPhaseStartTest "Test installed binaries"
+        rlRun "keylime_tenant --help"
+    rlPhaseEnd
+
+rlJournalEnd
