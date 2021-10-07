af5de4214e649bf6ab8c5f112b33fe1a287e8e9c
Karel Srot <kkaarreell@users.noreply.github.com>
2021-10-07 12:20:20 +0200

    add Library functions for excludelist management (#7)
    * add Library functions for excludelist management
    
    * do not add /keylime-tests to the default exclude list
    
    * simplify regular expressions used in exclude list

--- Library/test-helpers/lib.sh
+++ Library/test-helpers/lib.sh
@@ -66,6 +66,10 @@ export __INTERNAL_limeLogIMAEmulator
 export __INTERNAL_limeLogCurrentTest
 [ -n "$__INTERNAL_limeLogCurrentTest" ] || __INTERNAL_limeLogCurrentTest="$__INTERNAL_limeTmpDir/limeLib-current-test"
 
+export __INTERNAL_limeBaseExcludeList
+[ -n "$__INTERNAL_limeBaseExcludeList" ] || __INTERNAL_limeBaseExcludeList="$__INTERNAL_limeTmpDir/limeLib-base-exludelist"
+
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #   Functions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -442,10 +446,11 @@ true <<'=cut'
 =head2 limeCreateTestLists
 
 Creates allowlist.txt and excludelist.txt to be used for testing purposes.
-Allowlist would contain only initramdisk related content, all root dir / content
-will be added to excludelist. This is based on an assumption that content
-used for testing purposes will be created in / with an unique name later.
-from a given file.
+Allowlist will contain only initramdisk related content.
+Exclude list will contain all root dir / content except /keylime-tests.
+This is based on an assumption that content used for testing purposes will 
+be created under /keylime-tests in a directory with an unique name.
+See limeExtendNextExcludelist and limeCreateTestDir for more details.
 
     limeCreateTestLists
 
@@ -460,11 +465,90 @@ Returns 0 when the initialization was successfull, non-zero otherwise.
 limeCreateTestLists() {
 
     # generate allowlist
-    rlRun "bash $limeLibraryDir/create_allowlist.sh allowlist.txt sha256sum" && \
+    bash $limeLibraryDir/create_allowlist.sh allowlist.txt sha256sum && \
     # generate excludelist
-    rlRun "bash $limeLibraryDir/create_excludelist.sh excludelist.txt"
+    bash $limeLibraryDir/create_excludelist.sh excludelist.txt && \
+    # make sure the file exists
+    touch $__INTERNAL_limeBaseExcludeList && \
+    cat $__INTERNAL_limeBaseExcludeList >> excludelist.txt
 
 }
+
+true <<'=cut'
+=pod
+
+=head2 limeExtendNextExcludelist
+
+
+Stores provided paths to a list which gets added to the excludelist generated using limeCreateTestLists.
+Once a file not an a whitelist is measured by IMA it gets accounted until the next reboot.
+From this reason any file created by the test (even if they are deleted later) must be added to the
+exclude list to make sure that subsequent tests won't eventually fail attestation due to this file.
+
+    limeExtendNextExcludelist PATH1 [PATH2...]
+
+For test purposes it seems reasonable to store test files used for the attestation in a dedicated
+directory undre the /keylime-tests directory.
+You can generate unique directory e.g. using:
+
+    mkdir -p /keylime-tests
+    mktemp -d "/keylime-tests/test-name-XXXXX"
+
+or simply use the function
+
+    limeCreateTestDir
+
+Due to reasons above it seems unnecessary to removing such test files in test clean up phase.
+Keeping them in place would at least ensure unique directory name in subsequent test runs of
+the same test (e.g. when the test is parametrized).
+
+=over
+
+=item
+
+    PATH - path to be added to the future allowlist.
+
+=back
+
+Returns 0 when the execution was successfull, non-zero otherwise.
+
+=cut
+
+limeExtendNextExcludelist() {
+
+    for F in $@; do
+        echo "$F(/.*)?" >> $__INTERNAL_limeBaseExcludeList
+    done
+
+}
+
+true <<'=cut'
+=pod
+
+=head2 limeCreateTestDir
+
+Creates a directory under /keylime-tests directory with a unique name
+prefixed by the test name. Path of the created directory is printed to STDOUT.
+
+    limeCreateTestDir
+
+=over
+
+=back
+
+Returns 0 when the initialization was successfull, non-zero otherwise.
+
+=cut
+
+limeCreateTestDir() {
+
+    local TESTNAME && \
+    TESTNAME=$( basename $( cat $__INTERNAL_limeLogCurrentTest ) ) && \
+    mkdir -p /keylime-tests && \
+    mktemp -d "/keylime-tests/${TESTNAME}-XXXXX"
+
+}
+
 
 # ~~~~~~~~~~~~~~~~~~~~
 #   Logging
