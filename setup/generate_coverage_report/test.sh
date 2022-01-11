e05a772b84e89f151a2bb40e7addcd71ee0d7454
Karel Srot <ksrot@redhat.com>
2022-01-11 18:12:21 +0100

    added /setup/generate_coverage_report

---
+++ setup/generate_coverage_report/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+
+    rlPhaseStartSetup
+        rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
+        rlRun "which coverage"
+        rlRun "pushd $__INTERNAL_limeCoverageDir"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlRun "coverage combine"
+        rlAssertExists .coverage
+        rlRun "coverage html --include '*keylime*'"
+        rlRun "coverage report --include '*keylime*'"
+        rlRun "cd .."
+        rlRun "tar -czf coverage.tar.gz coverage"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlFileSubmit coverage.tar.gz
+        rlRun "popd"
+    rlPhaseEnd
+ 
+rlJournalEnd
