40a37b71c4bf7172747979c749f94d299bbfd657
Karel Srot <ksrot@redhat.com>
2022-01-25 22:13:50 +0100

    Modify revocation script to better handle own UUID

--- functional/basic-attestation-on-localhost/payload/local_action_modify_payload.py
+++ functional/basic-attestation-on-localhost/payload/local_action_modify_payload.py
@@ -3,6 +3,7 @@ import ast
 import keylime.secure_mount as secure_mount
 from keylime import keylime_logging
 from keylime import ca_util
+from keylime import config
 
 logger = keylime_logging.init_logging("local_action_rm_ssh")
 
@@ -10,20 +11,11 @@ async def execute(event):
     if event.get("type") != "revocation":
         return
 
-    metadata = event.get("meta_data", {})
-    if isinstance(metadata, str):
-        metadata = ast.literal_eval(metadata)
-
-    serial = metadata.get("cert_serial")
-    if serial is None:
-        logger.error("Unsupported revocation message: %s", event)
-
     # load up my own cert
-    uuid = event.get("agent_id", "my")
-    secdir = secure_mount.mount()
-    cert = ca_util.load_cert_by_path(f"{secdir}/unzipped/{uuid}-cert.crt")
+    event_uuid = event.get("agent_id", "my")
+    my_uuid = config.get('cloud_agent', 'agent_uuid')
     logger.info("A node in the network has been compromised: %s", event["ip"])
-    logger.info("my serial: %s, cert serial: %s" % (serial, cert.serial_number))
+    logger.info("my UUID: %s, event UUID: %s" % (my_uuid, event_uuid))
     # is this revocation meant for me?
-    if serial == cert.serial_number:
+    if my_uuid == event_uuid:
         os.remove("/var/tmp/test_payload_file")
