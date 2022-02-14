b93628b8328c0c233e4b2fcd762263aa1bc58a08
Karel Srot <ksrot@redhat.com>
2022-02-14 11:50:13 +0100

    Fix issue with early pip3 call

--- setup/install_upstream_keylime/test.sh
+++ setup/install_upstream_keylime/test.sh
@@ -22,9 +22,12 @@ enabled=1
 enabled_metadata=1
 _EOF'
             RHEL_EXTRA_PKGS="cfssl python3-pip"
-            rlRun "pip3 install lark-parser"
         fi
         rlRun "yum -y install $FEDORA_EXTRA_PKGS $RHEL_EXTRA_PKGS git-core python3-pip python3-pyyaml python3-tornado python3-simplejson python3-requests python3-sqlalchemy python3-alembic python3-packaging python3-psutil python3-gnupg python3-cryptography libselinux-python3 procps-ng tpm2-abrmd tpm2-tss tpm2-tools python3-zmq patch"
+        # need to install few more pgs from pip on RHEL
+        if rlIsRHEL; then
+            rlRun "pip3 install lark-parser"
+        fi
         if [ -d /var/tmp/keylime_sources ]; then
             rlLogInfo "Installing keylime from /var/tmp/keylime_sources"
         else
