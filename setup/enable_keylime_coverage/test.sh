6f757d112df841988368ec3275d154a91d3e7faa
Sergio Correia <scorreia@redhat.com>
2022-02-15 17:17:14 -0300

    setup/enable_keylime_coverage: try to fix python3-coverage version

--- setup/enable_keylime_coverage/test.sh
+++ setup/enable_keylime_coverage/test.sh
@@ -6,7 +6,7 @@ rlJournalStart
 
     rlPhaseStartSetup "Install coverage script its dependencies"
         rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
-        rlRun 'pip3 install coverage'
+        rlRun 'pip3 install coverage==5.5'
         rlRun "touch $__INTERNAL_limeCoverageDir/enabled"
     rlPhaseEnd
 
