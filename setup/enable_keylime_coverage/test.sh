2a998fbacbf5d7627b612d15622ee2ec1511af47
Karel Srot <ksrot@redhat.com>
2022-03-18 13:48:40 +0100

    Run upstream bits as keylime user and adjust coverage measurement

--- setup/enable_keylime_coverage/test.sh
+++ setup/enable_keylime_coverage/test.sh
@@ -14,4 +14,21 @@ rlJournalStart
         rlRun "coverage --help"
     rlPhaseEnd
 
+    rlPhaseStartSetup "Modify keylime systemd unit files"
+        id keylime && rlRun "chown -R keylime /var/tmp/limeLib && chmod -R g+w /var/tmp/limeLib"
+        for F in agent verifier registrar; do
+            rlRun "mkdir -p /etc/systemd/system/keylime_${F}.service.d"
+            rlRun "cat > /etc/systemd/system/keylime_${F}.service.d/10-coverage.conf <<_EOF
+[Service]
+# set variable containing name of the currently running test
+EnvironmentFile=/etc/systemd/limeLib.context
+# we need to change WorkingDirectory since .coverage* files will be stored there
+WorkingDirectory=/var/tmp/limeLib/coverage
+ExecStart=
+ExecStart=/usr/local/bin/coverage run -p --context \\\${limeCoverageContext} /usr/local/bin/keylime_${F}
+_EOF"
+        done
+	rlRun "systemctl daemon-reload"
+    rlPhaseEnd
+
 rlJournalEnd
