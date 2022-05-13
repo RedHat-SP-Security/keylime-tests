4de5e46864e944808adfe797fbe68b412dfc5d69
Karel Srot <ksrot@redhat.com>
2022-05-13 16:37:34 +0200

    Add test basic-attestation-with-ima-signatures

---
+++ functional/basic-attestation-with-ima-signatures/test.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+# This test requires HW TMP
+
+AGENT_ID="d432fbb3-d2f1-4a97-9ef7-75bd81c00000"
+AGENT_USER=kagent
+AGENT_GROUP=tss
+AGENT_WORKDIR=/var/lib/keylime-agent
+
+rlJournalStart
+
+    rlPhaseStartSetup "Do the keylime setup"
+        rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
+        # if TPM emulator is present, stop
+        limeTPMEmulated && rlDie "This test requires TPM device to be present since kernel boot"
+        rlAssertRpm keylime
+        limeBackupConfig
+        # update /etc/keylime.conf
+        rlRun "limeUpdateConf tenant require_ek_cert False"
+        rlRun "limeUpdateConf cloud_verifier revocation_notifier False"
+        rlRun "limeUpdateConf cloud_agent listen_notifications False"
+        # start keylime_verifier
+        rlRun "limeStartVerifier"
+        rlRun "limeWaitForVerifier"
+        rlRun "limeStartRegistrar"
+        rlRun "limeWaitForRegistrar"
+        rlRun "limeStartAgent"
+        rlRun "limeWaitForAgentRegistration ${AGENT_ID}"
+        # create allowlist and excludelist
+        limeCreateTestLists
+    rlPhaseEnd
+
+    rlPhaseStartTest "Add IMA signature to a test file"
+        TESTDIR=`limeCreateTestDir`
+        rlRun "chmod a+rx ${TESTDIR}"
+        SCRIPT="${TESTDIR}/echo"
+        rlRun "echo -e '#!/bin/bash\necho boom' > ${SCRIPT} && chmod a+x ${SCRIPT} && chown ${limeTestUser}.${limeTestUser} ${SCRIPT}"
+        ls -l ${SCRIPT}
+        #ALG_ARG="-a sha256"
+        rlRun "evmctl ima_sign ${ALG_ARG} ${SCRIPT}"
+        rlRun -s "getfattr -m ^security.ima --dump ${SCRIPT}"
+        rlRun "evmctl ima_verify ${ALG_ARG} ${SCRIPT}"
+        # if IMA is emulated, we would have good checksum for Agent but our running kernel would deny access anyway
+        rlRun -s "${SCRIPT} boom"
+        rlAssertGrep "boom" $rlRun_LOG
+        rlRun -s "grep '${SCRIPT}' /sys/kernel/security/ima/ascii_runtime_measurements"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Add keylime agent"
+        rlRun "lime_keylime_tenant -v 127.0.0.1 -t 127.0.0.1 -u ${AGENT_ID} --allowlist allowlist.txt --exclude excludelist.txt -f excludelist.txt --sign_verification_key ${limeIMAPublicKey} -c add"
+        rlRun "limeWaitForAgentStatus ${AGENT_ID} 'Get Quote'"
+        rlRun -s "lime_keylime_tenant -c cvlist"
+        rlAssertGrep "{'code': 200, 'status': 'Success', 'results': {'uuids':.*'${AGENT_ID}'" $rlRun_LOG -E
+    rlPhaseEnd
+
+    rlPhaseStartTest "Fail keylime agent"
+        rlRun "echo >> ${SCRIPT}"
+        rlRun "${SCRIPT}"
+        rlRun "limeWaitForAgentStatus ${AGENT_ID} '(Failed|Invalid Quote)'"
+        rlAssertGrep "WARNING - signature for file ${SCRIPT} is not valid" $(limeVerifierLogfile)
+        rlAssertGrep "ERROR - IMA ERRORS: Some entries couldn't be validated" $(limeVerifierLogfile)
+        rlAssertGrep "WARNING - Agent ${AGENT_ID} failed, stopping polling" $(limeVerifierLogfile)
+    rlPhaseEnd
+
+    rlPhaseStartCleanup "Do the keylime cleanup"
+        rlRun "limeStopAgent"
+        rlRun "limeStopRegistrar"
+        rlRun "limeStopVerifier"
+        limeLogfileSubmit $(limeVerifierLogfile)
+        limeLogfileSubmit $(limeRegistrarLogfile)
+        limeLogfileSubmit $(limeAgentLogfile)
+        limeClearData
+        limeRestoreConfig
+        limeExtendNextExcludelist ${TESTDIR}
+        #rlRun "rm -f $TESTDIR/keylime-bad-script.sh"  # possible but not really necessary
+    rlPhaseEnd
+
+rlJournalEnd
--- setup/configure_kernel_ima_module/test.sh
+++ setup/configure_kernel_ima_module/test.sh
@@ -2,8 +2,9 @@
 # vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-[ -z "$IMA_STATE" ] && IMA_STATE="on"
+[ -z "$IMA_APPRAISE" ] && IMA_APPRAISE="fix"
 [ -z "$IMA_POLICY" ] && IMA_POLICY="tcb"
+[ -z "$IMA_TEMPLATE" ] && IMA_TEMPLATE="ima-sig"
 
 COOKIE=/var/tmp/configure-kernel-ima-module-rebooted
 TESTFILE=/var/tmp/configure-kernel-ima-module-test$$
@@ -12,23 +13,30 @@ rlJournalStart
 
   if [ ! -e $COOKIE ]; then
     rlPhaseStartSetup "pre-reboot phase"
+        rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
         rlRun "grubby --info DEFAULT | grep '^args'"
-        rlRun "grubby --update-kernel DEFAULT --args 'ima=$IMA_STATE ima_policy=$IMA_POLICY'"
+        rlRun "grubby --update-kernel DEFAULT --args 'ima_appraise=${IMA_APPRAISE} ima_policy=${IMA_POLICY} ima_template=${IMA_TEMPLATE}'"
         rlRun -s "grubby --info DEFAULT | grep '^args'"
-        rlAssertGrep "ima=$IMA_STATE" $rlRun_LOG
-        rlAssertGrep "ima_policy=$IMA_POLICY" $rlRun_LOG
+        rlAssertGrep "ima_appraise=${IMA_APPRAISE}" $rlRun_LOG
+        rlAssertGrep "ima_policy=${IMA_POLICY}" $rlRun_LOG
+        rlAssertGrep "ima_template=${IMA_TEMPLATE}" $rlRun_LOG
         rlRun "touch $COOKIE"
-
+        # generate key and certificate for IMA
+        rlRun "limeInstallIMAKeys"
+        # install IMA policy
+        rlRun "limeInstallIMAConfig"
         # clear TPM
         rlRun "tpm2_clear"
-        rhts-reboot
     rlPhaseEnd
+
+    rhts-reboot
 
   else
     rlPhaseStartTest "post-reboot IMA test"
         rlRun -s "cat /proc/cmdline"
-        rlAssertGrep "ima=$IMA_STATE" $rlRun_LOG
-        rlAssertGrep "ima_policy=$IMA_POLICY" $rlRun_LOG
+        rlAssertGrep "ima_appraise=${IMA_APPRAISE}" $rlRun_LOG
+        rlAssertGrep "ima_policy=${IMA_POLICY}" $rlRun_LOG
+        rlAssertGrep "ima_template=${IMA_TEMPLATE}" $rlRun_LOG
         rlRun "rm $COOKIE"
 
         if [ "$IMA_STATE" == "on" -o "$IMA_STATE" == "1" ]; then
