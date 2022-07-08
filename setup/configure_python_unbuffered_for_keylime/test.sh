bfb8f008280bb0f9a7a1a320af071ff633e13ee6
Sergio Correia <scorreia@redhat.com>
2022-07-08 18:16:42 -0300

    Create directory for the service override
    As it may not exist.

--- setup/configure_python_unbuffered_for_keylime/test.sh
+++ setup/configure_python_unbuffered_for_keylime/test.sh
@@ -8,6 +8,7 @@ rlJournalStart
         # for more info see
         # https://unix.stackexchange.com/questions/505146/getting-systemd-service-logs-faster-from-my-service
         for SERVICE in verifier registrar agent; do
+            rlRun "mkdir -p /etc/systemd/system/keylime_${SERVICE}.service.d/"
             rlRun "cat > /etc/systemd/system/keylime_${SERVICE}.service.d/30-unbuffer.conf <<_EOF
 [Service]
 Environment=\"PYTHONUNBUFFERED=1\"
