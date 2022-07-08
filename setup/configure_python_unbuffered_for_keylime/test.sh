0bfef90a86459c8cd99288a5b2297a526bfec35e
Karel Srot <ksrot@redhat.com>
2022-07-08 22:48:37 +0200

    Add /setup/configure_python_unbuffered_for_keylime
    Sometimes there are test failures caused by the absence of a log
    message in a service log, however the same message can be seen
    in a log at the end of a test. Looks like a buffering problem.
    This setup task sets PYTHONUNBUFFERED=1 for keylime services
    as described at
    https://unix.stackexchange.com/questions/505146/getting-systemd-service-logs-faster-from-my-service

---
+++ setup/configure_python_unbuffered_for_keylime/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+
+    rlPhaseStartSetup
+        # for more info see
+        # https://unix.stackexchange.com/questions/505146/getting-systemd-service-logs-faster-from-my-service
+        for SERVICE in verifier registrar agent; do
+            rlRun "cat > /etc/systemd/system/keylime_${SERVICE}.service.d/30-unbuffer.conf <<_EOF
+[Service]
+Environment=\"PYTHONUNBUFFERED=1\"
+_EOF"
+        done
+        rlRun "systemctl daemon-reload"
+    rlPhaseEnd
+
+rlJournalEnd
