ac8b4692bef91a3fc4c14999894a5e2ea58fc859
Sergio Correia <scorreia@redhat.com>
2022-08-23 18:53:23 -0300

    Add python3-pyasn1 and python3-pyasn1-modules to the list of deps

--- setup/install_upstream_keylime/test.sh
+++ setup/install_upstream_keylime/test.sh
@@ -40,7 +40,7 @@ _EOF'
                 RHEL_EXTRA_PKGS="$RHEL_EXTRA_PKGS python3-packaging"
             fi
         fi
-        rlRun "yum -y install $FEDORA_EXTRA_PKGS $RHEL_EXTRA_PKGS git-core python3-pip python3-pyyaml python3-tornado python3-requests python3-sqlalchemy python3-alembic python3-psutil python3-gnupg python3-cryptography libselinux-python3 procps-ng tpm2-abrmd tpm2-tss tpm2-tools patch"
+        rlRun "yum -y install $FEDORA_EXTRA_PKGS $RHEL_EXTRA_PKGS git-core python3-pip python3-pyyaml python3-tornado python3-requests python3-sqlalchemy python3-alembic python3-psutil python3-gnupg python3-cryptography libselinux-python3 python3-pyasn1 python3-pyasn1-modules procps-ng tpm2-abrmd tpm2-tss tpm2-tools patch"
         if [ -z "$KEYLIME_TEST_DISABLE_REVOCATION" ]; then
             rlRun "yum -y install python3-zmq"
         fi
