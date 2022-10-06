1cafa4d5dbe37a120f34843f0e973bb8c5693b9c
Karel Srot <ksrot@redhat.com>
2022-10-06 16:49:50 +0200

    test, ignore

--- functional/basic-attestation-on-localhost/test.sh
+++ functional/basic-attestation-on-localhost/test.sh
@@ -2,116 +2,22 @@
 # vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-# set REVOCATION_NOTIFIER=zeromq to use the zeromq notifier
-[ -n "$REVOCATION_NOTIFIER" ] || REVOCATION_NOTIFIER=agent
-HTTP_SERVER_PORT=8080
-AGENT_ID="d432fbb3-d2f1-4a97-9ef7-75bd81c00000"
-
 rlJournalStart
 
     rlPhaseStartSetup "Do the keylime setup"
-        rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
-        rlAssertRpm keylime
 
-        # update /etc/keylime.conf
-        limeBackupConfig
-        # verifier
-        rlRun "limeUpdateConf revocations enabled_revocation_notifications '[\"${REVOCATION_NOTIFIER}\",\"webhook\"]'"
-        rlRun "limeUpdateConf revocations webhook_url http://localhost:${HTTP_SERVER_PORT}"
-        if [ -n "$KEYLIME_TEST_DISABLE_REVOCATION" ]; then
-            rlRun "limeUpdateConf revocations enabled_revocation_notifications '[]'"
-        fi
-        # tenant
-        rlRun "limeUpdateConf tenant require_ek_cert False"
-        # agent
-        if [ -n "$KEYLIME_TEST_DISABLE_REVOCATION" ]; then
-            rlRun "limeUpdateConf agent enable_revocation_notifications false"
-        fi
-        # if TPM emulator is present
-        if limeTPMEmulated; then
-            # start tpm emulator
-            rlRun "limeStartTPMEmulator"
-            rlRun "limeWaitForTPMEmulator"
-            # make sure tpm2-abrmd is running
-            rlServiceStart tpm2-abrmd
-            sleep 5
-            # start ima emulator
-            rlRun "limeInstallIMAConfig"
-            rlRun "limeStartIMAEmulator"
-        fi
-        sleep 5
-        # start keylime_verifier
-        rlRun "limeStartVerifier"
-        rlRun "limeWaitForVerifier"
-        rlRun "limeStartRegistrar"
-        rlRun "limeWaitForRegistrar"
-        rlRun "limeStartAgent"
-        rlRun "limeWaitForAgentRegistration ${AGENT_ID}"
-        # create allowlist and excludelist
-        limeCreateTestLists
-        HTTP_SERVER_LOG=$( mktemp )
-        # start revocation notifier webhook server using ncat
-        rlRun "ncat --no-shutdown -k -l ${HTTP_SERVER_PORT} -c '/usr/bin/sleep 3 && echo HTTP/1.1 200 OK' -o ${HTTP_SERVER_LOG} &"
-        HTTP_SERVER_PID=$!
-    rlPhaseEnd
+        rlRun "cat /proc/cmdline"
+        rlRun "sestatus"
+        rlRun "rpm -qa | grep selinux-policy"
+        rlRun "systemctl status auditd"
+        rlRun "id -Z"
+        rlRun "> /var/log/audit/audit.log"
+        rlRun "passwd --help > /root/file.txt" 
+        rlRun "cat /root/file.txt"
+        rlRun "ausearch -m avc"
+        rlRun "cat /var/log/audit/audit.log"
+        rlRun "dmesg"
 
-    rlPhaseStartTest "Add keylime agent"
-        REVOCATION_SCRIPT_TYPE=$( limeGetRevocationScriptType )
-        rlRun "cat > script.expect <<_EOF
-set timeout 20
-spawn keylime_tenant -v 127.0.0.1 -t 127.0.0.1 -u $AGENT_ID --verify --allowlist allowlist.txt --exclude excludelist.txt --include payload-${REVOCATION_SCRIPT_TYPE} --cert default -c add
-expect \"Please enter the password to decrypt your keystore:\"
-send \"keylime\n\"
-expect eof
-_EOF"
-        rlRun "expect script.expect"
-        rlRun "limeWaitForAgentStatus $AGENT_ID 'Get Quote'"
-        rlRun -s "keylime_tenant -c cvlist"
-        rlAssertGrep "{'code': 200, 'status': 'Success', 'results': {'uuids':.*'$AGENT_ID'" $rlRun_LOG -E
-        rlWaitForFile /var/tmp/test_payload_file -t 30 -d 1  # we may need to wait for it to appear a bit
-        ls -l /var/tmp/test_payload_file
-        rlAssertExists /var/tmp/test_payload_file
-    rlPhaseEnd
-
-    rlPhaseStartTest "Fail keylime agent"
-        TESTDIR=`limeCreateTestDir`
-        rlRun "echo -e '#!/bin/bash\necho boom' > $TESTDIR/keylime-bad-script.sh && chmod a+x $TESTDIR/keylime-bad-script.sh"
-        rlRun "$TESTDIR/keylime-bad-script.sh"
-        rlRun "rlWaitForCmd 'tail \$(limeVerifierLogfile) | grep -q \"Agent $AGENT_ID failed\"' -m 10 -d 1 -t 10"
-        rlRun "limeWaitForAgentStatus $AGENT_ID '(Failed|Invalid Quote)'"
-        rlAssertGrep "WARNING - File not found in allowlist: $TESTDIR/keylime-bad-script.sh" $(limeVerifierLogfile)
-        rlAssertGrep "WARNING - Agent $AGENT_ID failed, stopping polling" $(limeVerifierLogfile)
-        if [ -z "$KEYLIME_TEST_DISABLE_REVOCATION" ]; then
-            rlRun "rlWaitForCmd 'tail \$(limeAgentLogfile) | grep -q \"A node in the network has been compromised: 127.0.0.1\"' -m 10 -d 1 -t 10"
-            rlRun "tail -20 $(limeAgentLogfile) | grep 'Executing revocation action local_action_modify_payload'"
-            rlRun "tail $(limeAgentLogfile) | grep 'A node in the network has been compromised: 127.0.0.1'"
-            rlAssertNotExists /var/tmp/test_payload_file
-            cat ${HTTP_SERVER_LOG}
-            rlAssertGrep '\\"type\\": \\"revocation\\", \\"ip\\": \\"127.0.0.1\\", \\"agent_id\\": \\"d432fbb3-d2f1-4a97-9ef7-75bd81c00000\\"' ${HTTP_SERVER_LOG} -i
-            rlAssertNotGrep ERROR ${HTTP_SERVER_LOG} -i
-        fi
-    rlPhaseEnd
-
-    rlPhaseStartCleanup "Do the keylime cleanup"
-        rlRun "kill ${HTTP_SERVER_PID}"
-        rlRun "rm ${HTTP_SERVER_LOG}"
-        rlRun "rm -f /var/tmp/test_payload_file"
-        rlRun "limeStopAgent"
-        rlRun "limeStopRegistrar"
-        rlRun "limeStopVerifier"
-        limeLogfileSubmit $(limeVerifierLogfile)
-        limeLogfileSubmit $(limeRegistrarLogfile)
-        limeLogfileSubmit $(limeAgentLogfile)
-        if limeTPMEmulated; then
-            rlRun "limeStopIMAEmulator"
-            limeLogfileSubmit $(limeIMAEmulatorLogfile)
-            rlRun "limeStopTPMEmulator"
-            rlServiceRestore tpm2-abrmd
-        fi
-        limeClearData
-        limeRestoreConfig
-        limeExtendNextExcludelist $TESTDIR
-        #rlRun "rm -f $TESTDIR/keylime-bad-script.sh"  # possible but not really necessary
     rlPhaseEnd
 
 rlJournalEnd
