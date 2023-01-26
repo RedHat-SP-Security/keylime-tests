789ffe9080586afcb44d42bcdedb2443c6e86556
Karel Srot <ksrot@redhat.com>
2023-01-26 13:39:33 +0100

    update8

--- setup/configure_kernel_ima_module/test.sh
+++ setup/configure_kernel_ima_module/test.sh
@@ -15,10 +15,10 @@ rlJournalStart
   if [ ! -e $COOKIE ]; then
     rlPhaseStartSetup "pre-reboot phase"
         #rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
-        rlRun "grubby --info ALL"
-        rlRun "grubby --default-index"
+        #rlRun "grubby --info ALL"
+        #rlRun "grubby --default-index"
         #rlRun "grubby --update-kernel DEFAULT --args 'ima_appraise=${IMA_APPRAISE} ima_canonical_fmt ima_policy=${IMA_POLICY} ima_template=${IMA_TEMPLATE}'"
-        rlRun -s "grubby --info DEFAULT | grep '^args'"
+        #rlRun -s "grubby --info DEFAULT | grep '^args'"
         #rlAssertGrep "ima_appraise=${IMA_APPRAISE}" $rlRun_LOG
         #rlAssertGrep "ima_canonical_fmt" $rlRun_LOG
         #rlAssertGrep "ima_policy=${IMA_POLICY}" $rlRun_LOG
@@ -42,9 +42,9 @@ measure func=FILE_MMAP mask=MAY_EXEC
 measure func=MODULE_CHECK uid=0
 EOF"
         rlRun "cat policy > /sys/kernel/security/ima/policy"
-	rlRun "mkdir /etc/ima"
-        rlRun "cat policy > /etc/ima/ima-policy"
-        rlRun "restorecon -Rv /etc/ima"
+	#rlRun "mkdir /etc/ima"
+        #rlRun "cat policy > /etc/ima/ima-policy"
+        #rlRun "restorecon -Rv /etc/ima"
         # FIXME: workaround for issue https://github.com/keylime/keylime/issues/1025
         #rlRun "echo 'd /var/run/keylime 0700 keylime keylime' > /usr/lib/tmpfiles.d/keylime.conf"
     rlPhaseEnd
@@ -58,8 +58,8 @@ EOF"
         #rlAssertGrep "ima_canonical_fmt" $rlRun_LOG
         #rlAssertGrep "ima_policy=${IMA_POLICY}" $rlRun_LOG
         #rlAssertGrep "ima_template=${IMA_TEMPLATE}" $rlRun_LOG
-        rlRun "grubby --info ALL"
-        rlRun "grubby --default-index"
+        #rlRun "grubby --info ALL"
+        #rlRun "grubby --default-index"
         rlRun "rm $COOKIE"
 
         #if [ "${IMA_STATE}" == "on" -o "${IMA_STATE}" == "1" ]; then
