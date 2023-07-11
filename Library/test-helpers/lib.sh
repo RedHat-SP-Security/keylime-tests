23752eb495589990f572034807b5baa28472817d
Marcio Silva <marcio.a.silva@ibm.com>
2023-07-11 16:41:29 -0300

    Leverage JSON output on keylime_tenant
    Instead of grepping log messages in search of status/state/strings, use "jq"
    utility and select the specific required field on the JSON schema.
    
    Signed-off-by: Marcio Silva <marcio.a.silva@ibm.com>

--- Library/test-helpers/lib.sh
+++ Library/test-helpers/lib.sh
@@ -1167,7 +1167,8 @@ limeWaitForAgentStatus() {
 
     for I in `seq $TIMEOUT`; do
         keylime_tenant -c status -u $UUID &> $OUTPUT
-	if grep -E -q "\"operational_state\": \"$STATUS\"" $OUTPUT; then
+	AGTSTATE=$(cat "$OUTPUT" | grep ^{ | jq -r ".[].operational_state")
+	if [ $AGTSTATE == $STATUS] ; then
             cat $OUTPUT
 	    rm $OUTPUT
 	    return 0
@@ -1215,11 +1216,12 @@ limeWaitForAgentRegistration() {
 
     for I in `seq $TIMEOUT`; do
         keylime_tenant -c regstatus -u $UUID &> $OUTPUT
-	if grep -q "Agent $UUID exists on registrar" $OUTPUT; then
+        REGSTATE=$(cat $OUTPUT | grep ^{ | jq -r ".[].operational_state")
+	if [ $REGSTATE == "Registered" ]; then
             cat $OUTPUT
-	    rm $OUTPUT
-	    return 0
-	fi
+            rm $OUTPUT
+            return 0
+        fi
         sleep 1
     done
     cat $OUTPUT
