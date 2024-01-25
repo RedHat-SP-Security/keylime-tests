1f0af860abebf5f825510932d7a218f1d86ed0b8
Karel Srot <ksrot@redhat.com>
2024-01-25 09:43:02 +0100

    added strace

--- functional/basic-attestation-on-localhost/test.sh
+++ functional/basic-attestation-on-localhost/test.sh
@@ -38,9 +38,30 @@ rlJournalStart
             rlRun "limeStartIMAEmulator"
         fi
         sleep 5
+	rlRun "yum -y install strace"
+	rlRun "cat > /etc/systemd/system/keylime_verifier.service <<_EOF
+[Unit]
+Description=The Keylime verifier
+After=network.target
+Before=keylime_registrar.service
+StartLimitInterval=10s
+StartLimitBurst=5
+
+[Service]
+Group=keylime
+User=keylime
+ExecStart=/usr/bin/strace -f -o /var/tmp/strace.log /usr/bin/keylime_verifier
+Restart=on-failure
+TimeoutSec=60s
+RestartSec=120s
+
+[Install]
+WantedBy=default.target
+_EOF"
+        rlRun "systemctl restart"
         # start keylime_verifier
 	rlFileBackup /var/lib/keylime
-	for i in `seq 20`; do
+	#for i in `seq 3`; do
           rlRun "limeStartVerifier"
           rlRun "limeWaitForVerifier"
 	  rlRun "systemctl stop keylime_verifier"
@@ -48,86 +69,22 @@ rlJournalStart
 	  rlRun "rm -rf /var/lib/keylime/"
 	  rlFileRestore
 	  find /usr/lib/python3.9/site-packages/keylime -name '*.pyc' -exec rm {} \;
-	done
+#	done
           rlRun "limeStartVerifier"
           rlRun "limeWaitForVerifier"
-        rlRun "limeStartRegistrar"
-        rlRun "limeWaitForRegistrar"
-        rlRun "limeStartAgent"
-        rlRun "limeWaitForAgentRegistration ${AGENT_ID}"
-        # create some scripts
-        TESTDIR=`limeCreateTestDir`
-        rlRun "echo -e '#!/bin/bash\necho This is good-script1' > $TESTDIR/good-script1.sh && chmod a+x $TESTDIR/good-script1.sh"
-        rlRun "echo -e '#!/bin/bash\necho This is good-script2' > $TESTDIR/good-script2.sh && chmod a+x $TESTDIR/good-script2.sh"
-        # create allowlist and excludelist
-        rlRun "limeCreateTestPolicy ${TESTDIR}/*"
-        HTTP_SERVER_LOG=$( mktemp )
-        # start revocation notifier webhook server using ncat
-        rlRun "ncat --no-shutdown -k -l ${HTTP_SERVER_PORT} -c '/usr/bin/sleep 3 && echo HTTP/1.1 200 OK' -o ${HTTP_SERVER_LOG} &"
-        HTTP_SERVER_PID=$!
-    rlPhaseEnd
-
-    rlPhaseStartTest "Add keylime agent"
-        REVOCATION_SCRIPT_TYPE=$( limeGetRevocationScriptType )
-        rlRun "cat > script.expect <<_EOF
-set timeout 20
-spawn keylime_tenant -v 127.0.0.1 -t 127.0.0.1 -u $AGENT_ID --verify --runtime-policy policy.json --include payload-${REVOCATION_SCRIPT_TYPE} --cert default -c add
-expect \"Please enter the password to decrypt your keystore:\"
-send \"keylime\n\"
-expect eof
-_EOF"
-        rlRun "expect script.expect"
-        rlRun "limeWaitForAgentStatus $AGENT_ID 'Get Quote'"
-        rlRun -s "keylime_tenant -c cvlist"
-        rlAssertGrep "{'code': 200, 'status': 'Success', 'results': {'uuids':.*'$AGENT_ID'" $rlRun_LOG -E
-        rlWaitForFile /var/tmp/test_payload_file -t 30 -d 1  # we may need to wait for it to appear a bit
-        ls -l /var/tmp/test_payload_file
-        rlAssertExists /var/tmp/test_payload_file
-    rlPhaseEnd
-
-    rlPhaseStartTest "Running allowed scripts should not affect attestation"
-        rlRun "${TESTDIR}/good-script1.sh"
-        rlRun "${TESTDIR}/good-script2.sh"
-        rlRun "tail /sys/kernel/security/ima/ascii_runtime_measurements | grep good-script1.sh"
-        rlRun "tail /sys/kernel/security/ima/ascii_runtime_measurements | grep good-script2.sh"
-	rlRun "sleep 5"
-        rlRun "limeWaitForAgentStatus $AGENT_ID 'Get Quote'"
-    rlPhaseEnd
-
-    rlPhaseStartTest "Fail keylime agent"
-        rlRun "echo -e '#!/bin/bash\necho boom' > $TESTDIR/bad-script.sh && chmod a+x $TESTDIR/bad-script.sh"
-        rlRun "$TESTDIR/bad-script.sh"
-        rlRun "rlWaitForCmd 'tail \$(limeVerifierLogfile) | grep -q \"Agent $AGENT_ID failed\"' -m 10 -d 1 -t 10"
-        rlRun "limeWaitForAgentStatus $AGENT_ID '(Failed|Invalid Quote)'"
-        rlAssertGrep "WARNING - File not found in allowlist: $TESTDIR/bad-script.sh" $(limeVerifierLogfile)
-        rlAssertGrep "WARNING - Agent $AGENT_ID failed, stopping polling" $(limeVerifierLogfile)
-        if [ -z "$KEYLIME_TEST_DISABLE_REVOCATION" ]; then
-            rlRun "rlWaitForCmd 'tail \$(limeAgentLogfile) | grep -q \"A node in the network has been compromised: 127.0.0.1\"' -m 10 -d 1 -t 10"
-            rlRun "tail -20 $(limeAgentLogfile) | grep 'Executing revocation action local_action_modify_payload'"
-            rlRun "tail $(limeAgentLogfile) | grep 'A node in the network has been compromised: 127.0.0.1'"
-            rlAssertNotExists /var/tmp/test_payload_file
-            cat ${HTTP_SERVER_LOG}
-            rlAssertGrep '\\"type\\": \\"revocation\\", \\"ip\\": \\"127.0.0.1\\", \\"agent_id\\": \\"d432fbb3-d2f1-4a97-9ef7-75bd81c00000\\"' ${HTTP_SERVER_LOG} -i
-            rlAssertNotGrep ERROR ${HTTP_SERVER_LOG} -i
-        fi
     rlPhaseEnd
 
     rlPhaseStartCleanup "Do the keylime cleanup"
-        rlRun "kill ${HTTP_SERVER_PID}"
-        rlRun "rm ${HTTP_SERVER_LOG}"
-        rlRun "rm -f /var/tmp/test_payload_file"
-        rlRun "limeStopAgent"
-        rlRun "limeStopRegistrar"
         rlRun "limeStopVerifier"
         if limeTPMEmulated; then
             rlRun "limeStopIMAEmulator"
             rlRun "limeStopTPMEmulator"
             rlRun "limeCondStopAbrmd"
         fi
+	rlFileSubmit /var/tmp/strace.log
         limeSubmitCommonLogs
         limeClearData
         limeRestoreConfig
-        limeExtendNextExcludelist $TESTDIR
         #rlRun "rm -f $TESTDIR/*"  # possible but not really necessary
     rlPhaseEnd
 
