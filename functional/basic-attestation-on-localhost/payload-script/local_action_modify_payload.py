978ce5cd212190dc46c66a0912b7ddfca96d9501
Karel Srot <ksrot@redhat.com>
2024-04-05 13:25:38 +0200

    Replace python3-toml with tomli or tomllib modules

--- functional/basic-attestation-on-localhost/payload-script/local_action_modify_payload.py
+++ functional/basic-attestation-on-localhost/payload-script/local_action_modify_payload.py
@@ -3,7 +3,10 @@
 import os
 import sys
 import json
-import toml
+try:
+    import tomllib
+except ImportError:
+    import tomli as tomllib
 
 json_file = sys.argv[1]
 with open(json_file, 'r') as f:
@@ -14,8 +17,8 @@ if input_json.get("type", "") != "revocation":
 
 event_uuid = input_json.get("agent_id", "event_uuid")
 event_ip = input_json.get("ip", "event_ip")
-with open("/etc/keylime/agent.conf", "r") as f:
-    my_uuid = toml.load(f)["agent"]["uuid"].strip('\"')
+with open("/etc/keylime/agent.conf", "rb") as f:
+    my_uuid = tomllib.load(f)["agent"]["uuid"].strip('\"')
 
 print("A node in the network has been compromised:", event_ip)
 print("my UUID: %s, event UUID: %s" % (my_uuid, event_uuid))
