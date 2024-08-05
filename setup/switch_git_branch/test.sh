3cfb6410c5dd1d98ef53bfd8afa369f4f84adb9c
Karel Srot <ksrot@redhat.com>
2024-08-05 14:38:24 +0200

    Add rhel-10-main branch support

--- setup/switch_git_branch/test.sh
+++ setup/switch_git_branch/test.sh
@@ -12,9 +12,14 @@ rlJournalStart
         if [ -z "${SWITCH_TMT_TESTS_BRANCH}" ]; then
             rlLogInfo "No branch provided using SWITCH_TMT_TESTS_BRANCH variable, using autodetection"
             VERSION=$(rpm -q --qf "%{VERSION}" redhat-release)
+            MAJOR=$( echo $VERSION | cut -d '.' -f 1 )
+            rlLogInfo "VERSION=$VERSION"
+            rlLogInfo "MAJOR=$MAJOR"
             rlRun -s "git branch -r"
+            # try release specific branch
             SWITCH_TMT_TESTS_BRANCH=$( grep "origin/rhel-${VERSION}" "$rlRun_LOG" | head -1)
-            [ -z "$SWITCH_TMT_TESTS_BRANCH" ] && SWITCH_TMT_TESTS_BRANCH="rhel-9-main"
+            # try rhel-$MAJOR-main branch
+            [ -z "$SWITCH_TMT_TESTS_BRANCH" ] && SWITCH_TMT_TESTS_BRANCH=$( grep "origin/rhel-${MAJOR}-main" "$rlRun_LOG" | head -1)
             rlLogInfo "Autodetected branch: $SWITCH_TMT_TESTS_BRANCH"
         fi
         rlLogInfo "Switching to branch ${SWITCH_TMT_TESTS_BRANCH}"
