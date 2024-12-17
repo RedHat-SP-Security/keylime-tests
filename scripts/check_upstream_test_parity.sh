303a7ef4adb0fff230fe177b30f9aaef59be2495
Karel Srot <ksrot@redhat.com>
2024-12-17 08:17:17 +0100

    check_upstream_test_parity.sh: Print number of lines

--- scripts/check_upstream_test_parity.sh
+++ scripts/check_upstream_test_parity.sh
@@ -30,14 +30,17 @@ RESULT=0
 echo "Getting a list of available tests..."
 get_test_list > tests.txt
 get_agent_test_list > agent_tests.txt
+wc -l tests.txt agent_tests.txt
 echo "Getting a list of tests used in keylime CI..."
 get_upstream_tests "$KEYLIME_PLAN_URL" > keylime.txt
+wc -l keylime.txt
 echo
 do_diff agent_tests.txt keylime.txt
 RESULT=$(( RESULT+$? ))
 echo
 echo "Getting a list of tests used in rust-keylime CI..."
 get_upstream_tests "$AGENT_PLAN_URL" > agent.txt
+wc -l agent.txt
 echo
 do_diff agent_tests.txt agent.txt
 RESULT=$(( RESULT+$? ))
