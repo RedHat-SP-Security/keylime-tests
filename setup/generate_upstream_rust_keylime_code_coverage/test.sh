e8c21460219df647fbd8b7b452a56da04da1171f
Karel Srot <ksrot@redhat.com>
2025-01-17 17:30:54 +0100

    Drop uploadService related code

--- setup/generate_upstream_rust_keylime_code_coverage/test.sh
+++ setup/generate_upstream_rust_keylime_code_coverage/test.sh
@@ -2,12 +2,6 @@
 # vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
-
-# load functions to handle file upload
-. ../../scripts/upload_service.sh
-UPLOAD_SERVICE=$( uploadServiceFind )
-
-
 rlJournalStart
 
     rlPhaseStartSetup "Collect code coverage for rust components"
@@ -28,26 +22,6 @@ rlJournalStart
         rlFileSubmit e2e_coverage.tar.gz
         rlFileSubmit e2e_coverage.txt.tar.gz
         rlFileSubmit upstream_coverage.xml.tar.gz
-        if [ -f "e2e_coverage.tar.gz" ]; then
-            # upload e2e report in tar.gz
-            URL=$( uploadServiceUpload -v $UPLOAD_SERVICE e2e_coverage.tar.gz )
-            rlLogInfo "HTML code coverage report is available as GZIP archive at $URL"
-        fi
-        if [ -f "e2e_coverage.txt" ]; then
-            #upload e2e report in .txt format
-            URL=$( uploadServiceUpload -v $UPLOAD_SERVICE e2e_coverage.txt )
-            rlLogInfo "e2e_coverage.txt report is available at $URL"
-        fi
-        if [ -f "upstream_coverage.tar.gz" ]; then
-            #upload upstream report in .tar.gz
-            URL=$( uploadServiceUpload -v $UPLOAD_SERVICE upstream_coverage.tar.gz )
-            rlLogInfo "HTML code coverage report is available as GZIP archive at $URL"
-            fi
-        if [ -f "upstream_coverage.xml" ]; then
-            #upload
-            URL=$( uploadServiceUpload -v $UPLOAD_SERVICE upstream_coverage.xml )
-            rlLogInfo "upstream_coverage.xml report is available at $URL"
-        fi
         rlRun "popd"
     rlPhaseEnd
 
