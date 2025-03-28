ae06a334c2038f1974e5c2bb391b073586dd17d5
Karel Srot <ksrot@redhat.com>
2025-03-28 09:58:37 +0100

    Add 2 new sanity tests

---
+++ sanity/agent-service-start/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+
+    rlPhaseStartTest
+        rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
+        rlAssertRpm keylime
+
+        # if TPM emulator is present
+        if limeTPMEmulated; then
+            # start tpm emulator
+            rlRun "limeStartTPMEmulator"
+            rlRun "limeWaitForTPMEmulator"
+            rlRun "limeCondStartAbrmd"
+            # start ima emulator
+            rlRun "limeInstallIMAConfig"
+            rlRun "limeStartIMAEmulator"
+        fi
+        sleep 5
+        # start keylime_verifier
+	# we expect it to fail because the registrar is missing
+	# but at least we check that it got to the point of
+	# contacting the registrar
+        rlRun "limeStartAgent"
+	sleep 5
+	rlAssertGrep "Requesting registrar API version" "$(limeAgentLogfile)"
+        if limeTPMEmulated; then
+            rlRun "limeStopIMAEmulator"
+            rlRun "limeStopTPMEmulator"
+            rlRun "limeCondStopAbrmd"
+        fi
+        limeSubmitCommonLogs
+        limeClearData
+    rlPhaseEnd
+
+rlJournalEnd
