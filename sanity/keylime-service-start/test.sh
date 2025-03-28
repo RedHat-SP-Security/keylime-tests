c5341c7e9c61c6d613c4788df6d42fb72fbbe19c
Karel Srot <ksrot@redhat.com>
2025-03-28 09:58:37 +0100

    Add 2 new sanity tests

---
+++ sanity/keylime-service-start/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+
+    rlPhaseStartTest
+        rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
+        rlAssertRpm keylime
+        rlRun "limeStartVerifier"
+        rlRun "limeWaitForVerifier"
+        rlRun "limeStartRegistrar"
+        rlRun "limeWaitForRegistrar"
+        rlRun "limeStopRegistrar"
+        rlRun "limeStopVerifier"
+        limeSubmitCommonLogs
+        limeClearData
+    rlPhaseEnd
+
+rlJournalEnd
