a044271ac91f4e0eb6efd992c4ed91b0f895636d
Karel Srot <ksrot@redhat.com>
2025-06-11 16:11:58 +0200

    Support other architectures in yumrepogen_setup.sh

--- Library/test-helpers/yumrepogen_setup.sh
+++ Library/test-helpers/yumrepogen_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 pushd /root
-curl -kLo yumrepogen 'https://gitlab.cee.redhat.com/api/v4/projects/72924/jobs/artifacts/main/raw/yumrepogen-x86_64?job=compile'
+ARCH=$(arch)
+curl -kLo yumrepogen "https://gitlab.cee.redhat.com/api/v4/projects/72924/jobs/artifacts/main/raw/yumrepogen-$ARCH?job=compile"
 curl -kLo rhel.repo.tmpl 'https://gitlab.cee.redhat.com/testing-farm/yumrepogen/-/raw/main/rhel.repo.tmpl?inline=false'
 chmod a+x yumrepogen
-./yumrepogen -insecure -arch=x86_64 -compose-id=$(curl -kLs 'http://storage.tft.osci.redhat.com/composes-production.json' | grep -E -o "RHEL-9.6.0-[^\"]+" | head -1) -outfile /etc/yum.repos.d/yumrepogen.repo
+./yumrepogen -insecure -arch=$ARCH -compose-id=$(curl -kLs 'http://storage.tft.osci.redhat.com/composes-production.json' | grep -E -o "RHEL-9.6.0-[^\"]+" | head -1) -outfile /etc/yum.repos.d/yumrepogen.repo
 yum config-manager --set-enabled rhel-BaseOS --set-enabled rhel-AppStream
