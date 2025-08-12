723b260282200903b1f6cc7f8871edc42991f74c
Karel Srot <ksrot@redhat.com>
2025-08-12 13:01:34 +0200

    Fix pattern for keylime process detection
    Processes like keylime_ima_emulator do not have / before its name
    in process listing.

--- Library/test-helpers/lib.sh
+++ Library/test-helpers/lib.sh
@@ -491,7 +491,7 @@ __limeProcesses() {
     local NAME=$1
 
     pgrep -af "${NAME}([[:space:]]|\$)" \
-       | grep "/${NAME}" | grep -v grep
+       | grep "[^[:alnum:]-]${NAME}" | grep -v grep
 }
 
 __limePIDs() {
