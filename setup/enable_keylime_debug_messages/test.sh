2012c17349d4c08dc11417528357cf0361fa82f4
Sergio Correia <scorreia@redhat.com>
2025-11-19 21:05:56 +0000

    Update /setup/enable_keylime_debug_messages to use limeEnableDebugLog
    Signed-off-by: Sergio Correia <scorreia@redhat.com>

--- setup/enable_keylime_debug_messages/test.sh
+++ setup/enable_keylime_debug_messages/test.sh
@@ -6,17 +6,7 @@ rlJournalStart
 
     rlPhaseStartSetup
         rlRun 'rlImport "./test-helpers"' || rlDie "cannot import keylime-tests/test-helpers library"
-        rlRun "limeUpdateConf logger_root level DEBUG"
-        rlRun "limeUpdateConf logger_keylime level DEBUG"
-        rlRun "limeUpdateConf handler_consoleHandler level DEBUG"
-        for AGENT in keylime_agent keylime_push_model_agent; do
-            rlRun "mkdir -p /etc/systemd/system/${AGENT}.service.d"
-	    rlRun "cat > /etc/systemd/system/${AGENT}.service.d/20-rust_log_trace.conf <<_EOF
-[Service]
-Environment=\"RUST_LOG=keylime_agent=trace,keylime=trace\"
-_EOF"
-        done
-        rlRun "systemctl daemon-reload"
+        limeEnableDebugLog
     rlPhaseEnd
 
 rlJournalEnd
