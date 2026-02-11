fd3af7d983babe7b6f4e6969d3a33a4f219744b1
Karel Srot <ksrot@redhat.com>
2026-02-11 11:18:44 +0100

    Add new test

--- regression/cannot-use-mTLS-cert-on-server-admin-api-endpoint/test.sh
+++ regression/cannot-use-mTLS-cert-on-server-admin-api-endpoint/test.sh
@@ -7,6 +7,12 @@ AGENT_ID="d432fbb3-d2f1-4a97-9ef7-75bd81c00000"
 TENANT_ARGS=""
 [ "${AGENT_SERVICE}" == "PushAgent" ] && TENANT_ARGS="--push-model"
 VERIFIER_PORT=8881
+REGISTRAR_HTTP_PORT="8890"
+REGISTRAR_HTTPS_PORT="8891"
+[ -n "${API_VERSION}" ] || API_VERSION=2.4
+
+GOOD_CERTS="-cert /var/lib/keylime/cv_ca/client-cert.crt -key /var/lib/keylime/cv_ca/client-private.pem -CAfile /var/lib/keylime/cv_ca/cacert.crt"
+BAD_CERTS="-cert /var/lib/keylime/server-cert.crt -key /var/lib/keylime/server-private.pem -CAfile /var/lib/keylime/cv_ca/cacert.crt"
 
 rlJournalStart
 
@@ -30,12 +36,9 @@ rlJournalStart
             # Set the verifier to run in PUSH mode
             rlRun "limeUpdateConf verifier mode 'push'"
             rlRun "limeUpdateConf verifier challenge_lifetime 1800"
-            rlRun "limeUpdateConf verifier session_lifetime 180"
             rlRun "limeUpdateConf verifier quote_interval 10"
             rlRun "limeUpdateConf agent attestation_interval_seconds 10"
             rlRun "limeUpdateConf agent tls_accept_invalid_hostnames true"
-            rlRun "limeUpdateConf agent enable_authentication true"
-            rlRun "limeUpdateConf agent tls_accept_invalid_certs true"
         fi
         # if TPM emulator is present
         if limeTPMEmulated; then
@@ -67,26 +70,72 @@ rlJournalStart
         rlRun "limeCreateTestPolicy"
         # Add keylime agent"
         rlRun "keylime_tenant -u $AGENT_ID --runtime-policy policy.json -c add --file /etc/hostname ${TENANT_ARGS}"
-        rlRun "limeWaitForAgentStatus --field attestation_status $AGENT_ID PASS"
+        rlRun "limeWaitForAgentStatus --field attestation_status $AGENT_ID PASS" 0,1
     rlPhaseEnd
 
-    rlPhaseStartTest "Try to open TLS connection to the verifier using agent's mTLS cert"
-        GOOD_CERTS="-cert /var/lib/keylime/cv_ca/client-cert.crt -key /var/lib/keylime/cv_ca/client-private.pem -CAfile /var/lib/keylime/cv_ca/cacert.crt"
+    rlPhaseStartTest "Try to open TLS connection to the verifier on port ${VERIFIER_PORT} using agent's self-signed mTLS cert"
         rlRun "openssl x509 -text -in /var/lib/keylime/cv_ca/client-cert.crt"
         # use sleep to ensure that it won't be client closing the connection too early
         rlRun "sleep 1 | openssl s_client -connect 127.0.0.1:${VERIFIER_PORT} ${GOOD_CERTS} -verify_return_error" 0
         # keylime agent uses /var/lib/keylime/server-cert.crt which is what we are going to try
-        BAD_CERTS="-cert /var/lib/keylime/server-cert.crt -key /var/lib/keylime/server-private.pem -CAfile /var/lib/keylime/cv_ca/cacert.crt"
         rlRun "openssl x509 -text -in /var/lib/keylime/server-cert.crt"
         rlRun -s "sleep 1 | openssl s_client -connect 127.0.0.1:${VERIFIER_PORT} ${BAD_CERTS} -verify_return_error" 1
         rlAssertGrep "SSL alert number 48" "${rlRun_LOG}"
     rlPhaseEnd
 
-    rlPhaseStartTest "Try to use tenant with agent's self-signed mTLS certs"
+    rlPhaseStartTest "Try to use tenant with agent's self-signed mTLS certs against the verifier"
         rlRun "limeUpdateConf tenant tls_dir /var/lib/keylime"
         rlRun "limeUpdateConf tenant client_key server-private.pem"
         rlRun "limeUpdateConf tenant client_cert server-cert.crt"
+	rlRun -s "keylime_tenant -c cvlist" 1
+	rlAssertGrep "(TLSV1_ALERT_UNKNOWN_CA|Connection reset by peer|SSLError)" "$rlRun_LOG" -iE
 	rlRun "keylime_tenant -c delete --uuid ${AGENT_ID}" 1
+    rlPhaseEnd
+
+    rlPhaseStartTest "Try to use verifier admin API endpoint using curl"
+        rlRun -s "curl -kv https://127.0.0.1:${VERIFIER_PORT}/v${API_VERSION}/agents/"
+        rlAssertNotGrep "${AGENT_ID}" "$rlRun_LOG"
+        rlAssertGrep "403.*Forbidden.*Action list_agents requires admin authentication " "$rlRun_LOG" -iE
+        rlRun -s "curl -kv -X DELETE  https://127.0.0.1:${VERIFIER_PORT}/v${API_VERSION}/agents/${AGENT_ID}"
+        rlAssertNotGrep "(Accepted|Success)" "$rlRun_LOG" -iE
+        rlAssertGrep "403.*Forbidden.*Action delete_agent requires admin authentication " "$rlRun_LOG" -iE
+    rlPhaseEnd
+
+    rlPhaseStartTest "Try to open TLS connection to the registrar on port ${REGISTRAR_HTTPS_PORT} using agent's self-signed mTLS cert"
+        rlRun "openssl x509 -text -in /var/lib/keylime/cv_ca/client-cert.crt"
+        # use sleep to ensure that it won't be client closing the connection too early
+        rlRun "sleep 1 | openssl s_client -connect 127.0.0.1:${REGISTRAR_HTTPS_PORT} ${GOOD_CERTS} -verify_return_error" 0
+        # keylime agent uses /var/lib/keylime/server-cert.crt which is what we are going to try
+        rlRun "openssl x509 -text -in /var/lib/keylime/server-cert.crt"
+        rlRun -s "sleep 1 | openssl s_client -connect 127.0.0.1:${REGISTRAR_HTTPS_PORT} ${BAD_CERTS} -verify_return_error" 1
+        rlAssertGrep "SSL alert number 48" "${rlRun_LOG}"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Try to use tenant with agent's self-signed mTLS certs against the registrar"
+        # settings updated earlier while testing the verifier
+        #rlRun "limeUpdateConf tenant tls_dir /var/lib/keylime"
+        #rlRun "limeUpdateConf tenant client_key server-private.pem"
+        #rlRun "limeUpdateConf tenant client_cert server-cert.crt"
+	rlRun -s "keylime_tenant -c reglist" 1
+	rlAssertGrep "(TLSV1_ALERT_UNKNOWN_CA|Connection reset by peer|SSLError)" "$rlRun_LOG" -iE
+	rlRun "keylime_tenant -c regdelete --uuid ${AGENT_ID}" 1
+    rlPhaseEnd
+
+    rlPhaseStartTest "Try to use registrar admin API endpoint using curl on port ${REGISTRAR_HTTPS_PORT}"
+        rlRun -s "curl -kv https://127.0.0.1:${REGISTRAR_HTTPS_PORT}/v${API_VERSION}/agents/"
+        rlAssertNotGrep "${AGENT_ID}" "$rlRun_LOG"
+        rlAssertGrep "403.*Forbidden.*Action list_registrations requires admin authentication " "$rlRun_LOG" -iE
+        rlRun -s "curl -kv -X DELETE  https://127.0.0.1:${REGISTRAR_HTTPS_PORT}/v${API_VERSION}/agents/${AGENT_ID}"
+        rlAssertNotGrep "(Accepted|Success)" "$rlRun_LOG" -iE
+        rlAssertGrep "403.*Forbidden.*Action delete_registration requires admin authentication " "$rlRun_LOG" -iE
+    rlPhaseEnd
+
+    rlPhaseStartTest "Try to use registrar admin API endpoint using curl on port ${REGISTRAR_HTTP_PORT}"
+        # this should not work neither for HTTP nor HTTPS
+        rlRun -s "curl -kv --connect-timeout 3 http://127.0.0.1:${REGISTRAR_HTTP_PORT}/v${API_VERSION}/agents/" 0
+	rlAssertGrep "400 Bad Request" "$rlRun_LOG" -i
+        rlRun -s "curl -kv --connect-timeout 3 https://127.0.0.1:${REGISTRAR_HTTP_PORT}/v${API_VERSION}/agents/" 28,35
+        rlAssertNotGrep "${AGENT_ID}" "$rlRun_LOG"
     rlPhaseEnd
 
     rlPhaseStartCleanup "Do the keylime cleanup"
