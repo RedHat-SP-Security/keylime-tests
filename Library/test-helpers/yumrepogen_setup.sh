bd238269c77888d41174f5b4aeff6ba6bd7d91de
Karel Srot <ksrot@redhat.com>
2026-04-09 14:41:42 +0200

    fix

--- Library/test-helpers/yumrepogen_setup.sh
+++ Library/test-helpers/yumrepogen_setup.sh
@@ -5,5 +5,5 @@ ARCH=$(arch)
 curl -kLo yumrepogen "https://gitlab.cee.redhat.com/api/v4/projects/72924/jobs/artifacts/main/raw/yumrepogen-$ARCH?job=compile"
 curl -kLo rhel.repo.tmpl 'https://gitlab.cee.redhat.com/testing-farm/yumrepogen/-/raw/main/rhel.repo.tmpl?inline=false'
 chmod a+x yumrepogen
-./yumrepogen -insecure -arch=$ARCH -compose-id=$(curl -kLs 'http://storage.tft.osci.redhat.com/composes-production.json' | grep -E -o "RHEL-$( rpm -q --qf '%{VERSION}' redhat-release )[^\"]+" | head -1) -outfile /etc/yum.repos.d/yumrepogen.repo
+./yumrepogen -insecure -arch=$ARCH -compose-id=$(curl -kLs 'http://storage.tft.osci.redhat.com/composes-production.json' | grep -E -o "RHEL-$( rpm -q --qf '%{VERSION}' redhat-release )\.0[^\"]+" | head -1) -outfile /etc/yum.repos.d/yumrepogen.repo
 yum config-manager --set-enabled rhel-BaseOS --set-enabled rhel-AppStream
